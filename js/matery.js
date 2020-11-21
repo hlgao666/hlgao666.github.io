bced72896a89f2d54cf33c6f49df3f4dd60a9b5e
hlgao666 <2205958783@qq.com>
2020-11-21 16:24:25 +0800

    Site updated: 2020-11-21 16:24:24

--- js/matery.js
+++ js/matery.js
@@ -106,9 +106,9 @@ $(function () {
             subHtmlSelectorRelative: true
         });
 
-        // $(document).find('img[data-original]').each(function(){
-        //     $(this).parent().attr("href", $(this).attr("data-original"));
-        // });
+        $(document).find('img[data-original]').each(function(){
+            $(this).parent().attr("href", $(this).attr("data-original"));
+        });
 
         // progress bar init
         const progressElement = window.document.querySelector('.progress-bar');
